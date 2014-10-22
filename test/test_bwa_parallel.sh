16358be5b45de441e43603a0b69e957874e3042c
Eric T Dawson <eric.t.dawson@gmail.com>
2014-10-22 13:30:32 -0500

    Finished test script

--- test/test_bwa_parallel.sh
+++ test/test_bwa_parallel.sh
@@ -6,6 +6,8 @@
 #SBATCH -J test-BWA
 #SBATCH -o test-BWA-index.o%j
 
+module load pylauncher
+
 cd ..
 
 ##./build.sh
@@ -13,7 +15,7 @@ tar xzf bin.tgz
 
 ## Input (a fastq file)
 INFILE=$WORK/sandbox/ecoli_mda_lane1.fastq
-
+INDEX=${WORK}/sandbox/U00096.2.fas
 #Split it all up: 100K records per file
 echo "Time to split file into files of 100K records a piece"
 time python split.py -i ${INFILE} -r 100000
@@ -31,5 +33,13 @@ done
 python launcher.py -i commandfile -c 4
 
 
+head -n 120000 temp/split_0.fq >> ${OUTFILE}
+
+for i in temp/*.sam
+do
+    cat ${i} | grep -v "^@" >> ${OUTFILE}
+done
+
 
 rm -rf ./bin
+rm -rf temp
